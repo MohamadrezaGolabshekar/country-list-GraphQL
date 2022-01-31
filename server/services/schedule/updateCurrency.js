1a7910dd1f10b8047246495c4a0b65c7ef0bac77
MohamadrezaGolabshekar <m.golabshekar@gmail.com>
2022-01-31 11:57:14 +0330

    fix cron job time schedule

--- server/services/schedule/updateCurrency.js
+++ server/services/schedule/updateCurrency.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const setCurrencyRates = require("../currency/setCurrencyRates");
 
 // it runs every 8 hours and update currency rates
-const updateCurrencyTask = cron.schedule("* */8 * * *", async () => {
+const updateCurrencyTask = cron.schedule("0 0/8 * * *", async () => {
   console.log("currency rates is updated now");
   return await setCurrencyRates();
 });
